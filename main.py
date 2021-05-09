40a6c01844d2021e80e9edd3c0dd97595710bd40
Solovman <78027288+Solovman@users.noreply.github.com>
2021-05-09 13:59:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from telebot import *
+import config
+
+bot = TeleBot(config.token)
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    sticker = open('stickers/start.webp', 'rb')
+    bot.send_sticker(message.chat.id, sticker)
+    bot.send_message(message.chat.id, "Привет, {0.first_name}!\nменя зовут лягушка\
+ {1.first_name}.\nЯ - бот, созданный радовать людей".format(message.from_user, bot.get_me()), parse_mode='HTML')
+
+
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    bot.send_message(message.chat.id, "{0.first_name},\
+ чем могу тебе помочь?".format(message.from_user, bot.get_me()), parse_mode='HTML')
+
+
+@bot.message_handler(content_types=['text'])
+def echo_command(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
